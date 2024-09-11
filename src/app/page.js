6937e04600d03398eaf349ae2d5a83c9e1122a33
Denisse03 <99741771+Denisse03@users.noreply.github.com>
2024-09-10 20:38:50 -0400

    finish adding the images and remove the image folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 // "use client";
-// import Image from "next/image";
+import Image from "next/image";
 import homeStyle from "@/app/page.module.css";
+import photo from "../app/gradcap.jpeg";
+import photos from "../app/cap.jpeg";
+import pic from "../app/flower.jpeg";
 
 export default function Home() {
   const capStyle = {
-    backgroundColor: "red",
     width: "300px",
     height: "300px",
   };
@@ -36,17 +38,29 @@ export default function Home() {
       >
         <div style={capStyle} className="grad1">
           <h3> Graduation Cap Topper 1</h3>
+          <Image
+            src={photo}
+            style={{ width: "250px", height: "250px" }}
+          ></Image>
+
           <p>Price: $20.99</p>
           <button>Buy Now</button>
         </div>
         <div style={capStyle} className="grad2">
           <h3> Graduation Cap Topper 2</h3>
-          <p>Price: $20.99</p>
+          <Image
+            src={photos}
+            style={{ width: "250px", height: "250px" }}
+          ></Image>
+
+          <p>Price: $30.99</p>
           <button>Buy Now</button>
         </div>
         <div style={capStyle} className="grad3">
           <h3> Graduation Cap Topper 3</h3>
-          <p>Price: $20.99</p>
+          <Image src={pic} style={{ width: "250px", height: "250px" }}></Image>
+
+          <p>Price: $15.99</p>
           <button>Buy Now</button>
         </div>
       </div>
