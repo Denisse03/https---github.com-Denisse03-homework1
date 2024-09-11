17d5a0fb0d709d00031cf4a65ae9a6d47fb9bbed
Denisse03 <99741771+Denisse03@users.noreply.github.com>
2024-09-10 20:25:46 -0400

    finish styling and adding the information to the products

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 // "use client";
+// import Image from "next/image";
 import homeStyle from "@/app/page.module.css";
 
 export default function Home() {
-  const headerStyle = {
-    backgroundColor: "lightcoral",
-    height: "100px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  const capStyle = {
+    backgroundColor: "red",
+    width: "300px",
+    height: "300px",
   };
 
   return (
@@ -25,6 +24,32 @@ export default function Home() {
           </li>
         </ul>
       </nav>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+        className="gradCap"
+      >
+        <div style={capStyle} className="grad1">
+          <h3> Graduation Cap Topper 1</h3>
+          <p>Price: $20.99</p>
+          <button>Buy Now</button>
+        </div>
+        <div style={capStyle} className="grad2">
+          <h3> Graduation Cap Topper 2</h3>
+          <p>Price: $20.99</p>
+          <button>Buy Now</button>
+        </div>
+        <div style={capStyle} className="grad3">
+          <h3> Graduation Cap Topper 3</h3>
+          <p>Price: $20.99</p>
+          <button>Buy Now</button>
+        </div>
+      </div>
     </main>
   );
 }
